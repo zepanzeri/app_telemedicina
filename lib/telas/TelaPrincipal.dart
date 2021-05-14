3f5a588daa357190a5add03aef8e867b6411ebc3
zepanzeri <zepanzeri94@gmail.com>
2021-05-14 12:07:31 -0300

    Configurando o app para usar o Google Firebase.

--- lib/telas/TelaPrincipal.dart
+++ lib/telas/TelaPrincipal.dart
@@ -4,67 +4,69 @@ class TelaPrincipal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(60),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-               Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  height: 250,
-                  width: 250,
-                  child: Image.asset('assets/images/logo.png')
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  height: 50,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    child: Text('Login'),
-                    style: Theme.of(context).elevatedButtonTheme.style,
-                    onPressed: () {
-                      Navigator.pushNamed(
-                        context,
-                        '/tela_login');
-                    },
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                 Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 250,
+                    width: 250,
+                    child: Image.asset('assets/images/logo.png')
                   ),
                 ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  height: 50,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      child: Text('Criar conta'),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      child: Text('Login'),
+                      style: Theme.of(context).elevatedButtonTheme.style,
                       onPressed: () {
                         Navigator.pushNamed(
                           context,
-                          '/tela_criar_conta');
-                      }),
+                          '/tela_login');
+                      },
+                    ),
+                  ),
                 ),
-              ),
 
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  height: 50,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      child: Text('Sobre'),
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          '/tela_sobre');
-                      }),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        child: Text('Criar conta'),
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            '/tela_criar_conta');
+                        }),
+                  ),
                 ),
-              ),
-            ],
+
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        child: Text('Sobre'),
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            '/tela_sobre');
+                        }),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
